f38ce9e35b4966c0833117b668bbff411cbbd46a
Rahmat Basuki <awantengah@gmail.com>
2015-04-09 17:51:43 +0700

    styling layout

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CRUD MYSQLI</title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <header>
+            <div class="title">
+                <a href="index.php"><h1>CRUD MySQLi</h1></a>
+            </div>
+            <nav class="nav">
+                <ul>
+                    <li><a href="index.php" class="link">Home</a></li>
+                    <li><a href="insert.php" class="link">Insert</a></li>
+                </ul>
+            </nav>
+        </header>
--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>CRUD MYSQLI</title>
-    </head>
-    <body>
-        <header>
-            <h1>This is just simple crud using mysqli without design layout</h1>
-        </header>
-        <section>
-            <a href="insert.php">Insert</a>
-        </section>
-    </body>
-</html>
+<?php include 'header.php'; ?>
+<section>
+    
+</section>
+<?php include 'footer.php'; ?>
